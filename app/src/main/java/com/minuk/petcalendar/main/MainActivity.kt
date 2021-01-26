8542d0b64dfd5bbffd0de5945a7aeed0467a8925
jaeryo2357 <jaeryo2357@naver.com>
2021-01-27 00:13:36 +0900

    refactor: Navigation Component로 Fragment 전환하는 것으로 변경

--- app/src/main/java/com/minuk/petcalendar/main/MainActivity.kt
+++ app/src/main/java/com/minuk/petcalendar/main/MainActivity.kt
@@ -1,86 +1,25 @@
 package com.minuk.petcalendar.main
 
-import androidx.activity.viewModels
-import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 
 import com.minuk.petcalendar.R
 import com.minuk.petcalendar.base.BaseActivity
-import com.minuk.petcalendar.calendar.CalendarFragment
 import com.minuk.petcalendar.databinding.ActivityMainBinding
-import com.minuk.petcalendar.document.DocumentFragment
-import com.minuk.petcalendar.setting.SettingFragment
 
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private val mainViewModel: MainViewModel by viewModels()
-
     override fun initLayout() {
         initBottomNavigationView()
     }
 
-    override fun observeViewModel() {
-        observeNavigation()
-        observeToast()
-    }
-
     private fun initBottomNavigationView() {
-        binding.mainNavigation.setOnNavigationItemSelectedListener {
-            val mainUiType = when (it.itemId) {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
 
-                R.id.action_calendar -> MainUiType.CALENDAR
-
-                R.id.action_document -> MainUiType.DOCUMENT
-
-                else -> MainUiType.SETTING
-            }
-
-            mainViewModel.changeMainType(mainUiType)
-            true
-        }
-    }
-
-    private fun observeNavigation() {
-        mainViewModel.navigateSubject
-            .subscribe(::navigate)
-            .addToDisposable()
-    }
-
-    private fun observeToast() {
-        mainViewModel.toastSubject
-            .subscribe(::showToast)
-            .addToDisposable()
-    }
-
-    private fun navigate(type: MainUiType) {
-        val newFragment = supportFragmentManager.findFragmentByTag(type.tag)
-            ?: getFragment(type)
-
-        val currentFragment = supportFragmentManager.primaryNavigationFragment
-
-        if (currentFragment == null || currentFragment.javaClass != newFragment.javaClass) {
-            supportFragmentManager.commit {
-                setPrimaryNavigationFragment(newFragment)
-                replace(R.id.main_container, newFragment, type.tag)
-                addToBackStack(null)
-            }
-        }
-    }
-
-    private fun getFragment(type: MainUiType) = when (type) {
-        MainUiType.CALENDAR -> CalendarFragment.newInstance()
-
-        MainUiType.DOCUMENT -> DocumentFragment.newInstance()
-
-        else -> SettingFragment.newInstance()
-    }
-
-    /**
-     *  BackStack에 쌓인 Fragment 상관없이 뒤로 가기 버튼 누르면 바로 종료
-     */
-    override fun onBackPressed() {
-        finish()
+        binding.mainNavigation.setupWithNavController(navHostFragment.navController)
     }
 }
--- app/src/main/java/com/minuk/petcalendar/main/MainUiType.kt
+++
@@ -1,7 +0,0 @@
-package com.minuk.petcalendar.main
-
-enum class MainUiType(val tag: String) {
-    CALENDAR("fragment_calendar"),
-    DOCUMENT("fragment_document"),
-    SETTING("fragment_setting")
-}
